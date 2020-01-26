040626906be696701feb35962fa977580b8a8420
Родион <rad-62301@mail.ru>
2020-01-26 17:25:31 +0600

    Немного подправил код

--- Converter HW_3/AppDelegate.swift
+++ Converter HW_3/AppDelegate.swift
@@ -14,9 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         RateManager.incrementCount()//запуск менеджера
         
-                
         return true
     }
 
